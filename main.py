89bc293bc697259d4597dea4ae2f035fb5469db6
svsm03 <svsm03@hotmail.com>
2023-04-05 18:49:32 -0600

    Final Version

--- main.py
+++ main.py
@@ -10,7 +10,6 @@ import wikipedia
 import unidecode
 import subprocess as sub
 import csv
-import config
 
 eel.init("web")
 
@@ -21,24 +20,74 @@ engine = pyttsx3.init()
 voices = engine.getProperty("voices")
 engine.setProperty("voice", voices[0].id)
 
-def enviar_correo(destinatario, asunto, mensaje):
+def enviar_correo(comando):
     try:
         correo = "svsm2403@gmail.com"
 
         #CLAVE GENERADO POR GOOGLE GUARDADA COMO VARIABLE DE ENTORNO PARA MAYOR SEGURIDAD
         clave = os.environ.get("EMAIL_CLAVE")
         #PROCEDIMIENTO PARA ENVIAR CORREOS (LIBRERIA: email.message)
+
+        with open('contactos.csv', mode='r') as infile:
+            reader = csv.reader(infile)
+            contactos = {rows[0]:rows[1] for rows in reader}
+
+
+        for contacto in contactos:
+            if contacto in comando:
+                destinatario = contacto
+
         em = EmailMessage()
         em["From"] = correo
-        em["To"] = destinatario
+        em["To"] = contactos[destinatario]
+
+
+        with sr.Microphone() as source:
+            print("Escuchando...")
+            engine.say("Cual es el asunto?")
+            engine.runAndWait()
+            audio = r.listen(source)
+
+            try:
+                comando = r.recognize_google(audio, language="es-MX")
+                print(f"Asunto: {comando}")
+                engine.say(f"Entendido. {comando}")
+                engine.runAndWait()
+                asunto = comando
+
+            except Exception as e:
+                print(f"No se pudo reconocer el comando: {e}")
+                engine.say("No se pudo reconocer el asunto")
+                engine.runAndWait()
+
+
+        with sr.Microphone() as source:
+            print("Escuchando...")
+            engine.say("Cual es el mensaje?")
+            engine.runAndWait()
+            audio = r.listen(source)
+
+            try:
+                comando = r.recognize_google(audio, language="es-MX")
+                print(f"Mensaje: {comando}")
+                engine.say(f"Entendido. {comando}")
+                engine.runAndWait()
+                mensaje = comando
+
+            except Exception as e:
+                print(f"No se pudo reconocer el comando: {e}")
+                engine.say("No se pudo reconocer el mensaje")
+                engine.runAndWait()
+
         em["Subject"] = asunto
         em.set_content(mensaje)
 
         contexto = ssl.create_default_context()
         with smtplib.SMTP_SSL("smtp.gmail.com", 465, context=contexto) as smtp:
             smtp.login(correo,clave)
-            smtp.sendmail(correo, destinatario, em.as_string())
+            smtp.sendmail(correo, contactos[destinatario], em.as_string())
 
+        print("Correo enviado")
         engine.say("Correo enviado")
         engine.runAndWait()
     except Exception as e:
@@ -75,7 +124,6 @@ def musica(comando):
     engine.say("Reproduciendo" + music)
     engine.runAndWait()
     pywhatkit.playonyt(music)
-
     
 def wiki(comando):
     search = comando
@@ -97,7 +145,6 @@ def wiki(comando):
     print(search + ": "+ wiki)
     engine.say(wiki)
     engine.runAndWait()
-
 
 def write(f,com):
     com = com.replace("escribe", "")
@@ -126,10 +173,7 @@ def asistente():
             comando = unidecode.unidecode(comando)
             
             if "correo" in comando:
-                destinatario = input("Indica el correo del destinatario: ")
-                asunto = input("Indica el asunto del correo: ")
-                mensaje = input("Indica el mensaje del correo: ")
-                enviar_correo(destinatario, asunto, mensaje)
+                enviar_correo(comando)
 
 
             #PALABRAS QUE MANDAN A LLAMAR LA BUSQUEDA EN WIKIPEDIA
@@ -171,7 +215,10 @@ def asistente():
 
             #SOLAMENTE PASA EL COMANDO
             elif "adios" in comando:
-                print("")
+                print("Accion cancelada")
+
+            elif "nada" in comando:
+                print("Accion cancelada")
 
             else:
                 engine.say("Comando no reconocido")
@@ -181,6 +228,26 @@ def asistente():
             print(f"No se pudo reconocer el comando: {e}")
             engine.say("No se pudo reconocer el comando")
             engine.runAndWait()
+
+
+@eel.expose
+def agregar_aplicacion(x,y):
+    list_data = [x, y]
+    with open('programas.csv', 'a', newline='') as fd:
+        csv_writer = csv.writer(fd)
+        csv_writer.writerow(list_data)
+
+    return "Aplicacion agregada correctamente"
+
+@eel.expose
+def agregar_contacto(x,y):
+    list_data = [x, y]
+    with open('contactos.csv', 'a', newline='') as fd:
+        csv_writer = csv.writer(fd)
+        csv_writer.writerow(list_data)
+
+    return "Contacto agregado correctamente"
+
 
 @eel.expose
 def main():
--- web/index.js
+++ web/index.js
@@ -3,8 +3,13 @@ function runPython(){
 }
 
 
-function agregar(n,r){
+function agregarapp(n,r){
     eel.agregar_aplicacion(n,r)(function(ret){console.log(ret)});
+}
+
+
+function agregarcontacto(n,r){
+  eel.agregar_contacto(n,r)(function(ret){console.log(ret)});
 }
 
 function mostrarPopUp() {
@@ -20,9 +25,10 @@ function enviarDatos(event) {
     event.preventDefault(); // Prevenir el envío del formulario
     var nombre = document.getElementById("nombre").value;
     var ruta = document.getElementById("ruta").value;
+    nombre = nombre.toLowerCase();
     // Haz lo que quieras con los valores obtenidos
     console.log("Nombre: " + nombre + ", Ruta: " + ruta);
-    agregar(nombre,ruta);
+    agregarapp(nombre,ruta);
 
     // Limpia los campos de entrada
     document.getElementById("nombre").value = "";
@@ -34,3 +40,56 @@ function enviarDatos(event) {
     // Limpia el formulario
     document.getElementById("miFormulario").reset();
   }
+
+
+  function enviarDatoscontact(event) {
+    event.preventDefault(); // Prevenir el envío del formulario
+    var nombre = document.getElementById("nombre2").value;
+    var correo = document.getElementById("correo").value;
+    nombre = nombre.toLowerCase();
+    // Haz lo que quieras con los valores obtenidos
+    console.log("Nombre: " + nombre + ", Correo: " + correo);
+    agregarcontacto(nombre,correo);
+
+    // Limpia los campos de entrada
+    document.getElementById("nombre2").value = "";
+    document.getElementById("correo").value = "";
+
+    // Muestra el pop-up
+    mostrarPopUp();
+
+    // Limpia el formulario
+    document.getElementById("miFormulario2").reset();
+  }
+
+
+  function cambiarPagina() {
+    var switchElem = document.getElementById("mySwitch");
+    if (switchElem.checked) {
+      // Si el switch está activado, almacena el estado en el almacenamiento local
+      localStorage.setItem("switchState", "on");
+      // Redirige a la página 1
+      window.location.href = "settingsblack.html";
+    } else {
+      // Si el switch está desactivado, almacena el estado en el almacenamiento local
+      localStorage.setItem("switchState", "off");
+      // Redirige a la página 2
+      window.location.href = "settings.html";
+    }
+  }
+  
+  // Función que se ejecuta cuando se carga la página
+  function cargarPagina() {
+    // Obtiene el estado almacenado en el almacenamiento local
+    var switchState = localStorage.getItem("switchState");
+    if (switchState === "on") {
+      // Si el estado es "on", activa el switch
+      document.getElementById("mySwitch").checked = true;
+    } else {
+      // Si el estado es "off", desactiva el switch
+      document.getElementById("mySwitch").checked = false;
+    }
+  }
+  
+  // Registra la función cargarPagina para que se ejecute cuando se carga la página
+  window.onload = cargarPagina;
